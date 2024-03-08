37b74e5fa9383e7637df6f963702657461d655b4
unknown <reginadivadharma@gmail.com>
2024-03-08 13:14:50 +0700

    Add backend connection folder, also fixed git repo

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 5306,
+    database: 'shoup'
+});
+
+pool.query('SELECT * FROM user', (error, results) => {
+    if (error) {
+        console.error('Error executing query:', error);
+        return;
+    }
+    console.log('Query results:', results);
+});
+
+// Don't forget to close the pool when done (if necessary)
+// pool.end();
